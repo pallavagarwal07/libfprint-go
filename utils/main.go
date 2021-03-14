ae9def94778c513074434293bd51679edc46bc92
Pallav Agarwal <pallavagarwal07@gmail.com>
2021-03-14 16:15:22 +0530

    Re-organize files and add sample utility.

---
+++ utils/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pallavagarwal07/libfprint-go/fprint"
+)
+
+func app() error {
+	conn, err := fprint.NewConn( /* empty indicates current user */ "")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	fingers, err := conn.ListEnrolledFingers()
+	if err != nil {
+		return err
+	}
+	fmt.Println(fingers)
+
+	ch, err := conn.StartEnroll("right-index-finger")
+	if err != nil {
+		return err
+	}
+
+	for msg := range ch {
+		fmt.Println(msg)
+	}
+	return nil
+}
+
+func main() {
+	if err := app(); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
